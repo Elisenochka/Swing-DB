f18929c850d3bb7d9d25019871d3778de7dfd3e7
Elina <eli.lisenochka@gmail.com>
2016-04-05 22:15:03 +0300

    corrected

--- MainClass.java
+++ MainClass.java
@@ -5,23 +5,11 @@ import java.sql.*;
 
 
 public class MainClass {
-    public static Connection cnn;
-    public static Statement stnt;
-    public static ResultSet rsSt;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        cnn.Cntn();
+        DataBase.Cntn();
 
         MyWindow mw = new MyWindow();
-
-
-    public static void Cntn() throws ClassNotFoundException, SQLException {
-        cnn = null;
-        Class.forName("org.sqlite.JDBC");
-        cnn = DriverManager.getConnection("jdbc:sqlte:Swing.db");
-
-        System.out.println("Base is connected");
-    }
 
 
 }
