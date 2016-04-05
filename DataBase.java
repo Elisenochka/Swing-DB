a22ece8dac397ab7fc9617740537db80ee0e4989
Elina <eli.lisenochka@gmail.com>
2016-04-05 22:15:15 +0300

    creation added

--- DataBase.java
+++ DataBase.java
@@ -12,14 +12,19 @@ public class DataBase {
     public static ResultSet rsSt;
 
 
-
-
     public final static void Cntn() throws ClassNotFoundException, SQLException{
         cnn = null;
         Class.forName("org.sqlite.JDBC");
         cnn = DriverManager.getConnection("jdbc:sqlte:Swing.db");
 
         System.out.println("Base is connected");
+    }
+
+    public static void CrtDB(String name) throws ClassNotFoundException, SQLException{
+        stnt = cnn.createStatement();
+        stnt.execute("CREATE TABLE if not exsists '%s' ('ID','Name')");
+
+        System.out.println("DataBase is created");
     }
 
 
