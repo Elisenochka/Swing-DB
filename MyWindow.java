eb4946c7ded8dd5f3bc29e60a721d8ff32509d4b
Elina <eli.lisenochka@gmail.com>
2016-04-05 16:41:21 +0300

    creating panels and buttons

---
+++ MyWindow.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.sql.*;
+
+/**
+ * Created by Romych on 01.04.2016.
+ */
+public class MyWindow extends JFrame {
+
+    public MyWindow(){
+        setTitle("My first window");
+        setSize(400,400);
+        setLocation(1200,200);
+        //setLayout(new BorderLayout(0,20));
+        //setBackground(Color.BLACK);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+        JButton jb1 = new JButton("Send");
+        //add(jb1, BorderLayout.NORTH);
+        //JButton jb2 = new JButton("JButtton #2");
+        //add(jb2, BorderLayout.SOUTH);
+        final JTextArea jta = new JTextArea();
+        jta.setFont(new Font("Times New Roman", Font.ITALIC,14));
+        //add(jta, BorderLayout.CENTER);
+        jta.setBackground(Color.lightGray);
+        add(jta);
+        final JTextField jtf = new JTextField();
+        JTable jt = new JTable();
+        JPanel northPanel = new JPanel();
+        northPanel.setLayout(new BorderLayout());
+        JPanel southPanel = new JPanel();
+        southPanel.setLayout(new BorderLayout());
+        add(southPanel,BorderLayout.SOUTH);
+        add(northPanel,BorderLayout.NORTH);
+        southPanel.setLayout(new BorderLayout());
+        southPanel.add(jtf, BorderLayout.CENTER);
+        northPanel.add(jt, BorderLayout.CENTER);
+        southPanel.add(jb1, BorderLayout.EAST);
+        jb1.setMnemonic(KeyEvent.KEY_PRESSED);
+        //JRootPane rp = SwingUtilities.getRootPane(jb1);
+        //rp.setDefaultButton(jb1);
+        jb1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jta.append(jtf.getText() + '\n');
+                jtf.setText("");
+            }
+        });
+
+        setVisible(true);
+
+
+    }
+
+}
