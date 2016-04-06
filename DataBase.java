729b37f08f18d00843be31a965f5ff9af3bccebe
Elina <eli.lisenochka@gmail.com>
2016-04-06 23:12:30 +0300

    statement created

--- DataBase.java
+++ DataBase.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Executor;
@@ -22,20 +23,28 @@ public class DataBase {
     public final static void Cntn() throws ClassNotFoundException, SQLException{
         cnn = null;
         Class.forName("org.sqlite.JDBC");
-        cnn = DriverManager.getConnection("jdbc:sqlte:Swing.db");
+        cnn = DriverManager.getConnection("jdbc:sqlite:Swing.db");
+        stnt = cnn.createStatement();
 
         System.out.println("Base is connected");
     }
 
     public static void CrtDB(String name) throws ClassNotFoundException, SQLException{
-        stnt = cnn.createStatement();
-        stnt.execute("CREATE TABLE if not exsists "+name+ "('ID','Name')");
+
+        stnt.execute("CREATE TABLE if not exsists "+name+ "('ID','Login','First name','Name','Last name', 'Number')");
 
         System.out.println("DataBase is created");
     }
 
     public static void WriteDB(String name, String values[]) throws SQLException{
-        stnt.execute("INSERT INTO "+ name+"('Login','First name','Name','Last name', 'Number') VALUES ("+values.toString());
+        stnt.execute("INSERT INTO "+ name+"('Login','First name','Name','Last name', 'Number') VALUES ("+ Arrays.toString(values)+")");
+
+        System.out.println("Table is filled");
+
+    }
+
+    public static void WriteDB(String name) throws SQLException{
+        stnt.execute("INSERT INTO "+ name+"('Login','First name','Name','Last name', 'Number') VALUES ('fis','Fire','Indis','Same','23874904')");
 
         System.out.println("Table is filled");
 
