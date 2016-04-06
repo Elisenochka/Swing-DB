018b7d7952556897bfce96dc78a377464209ac90
Elina <eli.lisenochka@gmail.com>
2016-04-06 19:15:30 +0300

    tried to write bd

--- MainClass.java
+++ MainClass.java
@@ -1,13 +1,27 @@
 /**
  * Created by Romych on 01.04.2016.
  */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.*;
 
 
 public class MainClass {
+    static String name = "Swing.db";
+
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static String[] values = new String[5];
+
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         DataBase.Cntn();
+        values[0]="fis";
+        values[1]="From";
+        values[2]="Ire";
+        values[3]="Som";
+        values[4]="890823";
+        DataBase.WriteDB(name,values);
+
 
         MyWindow mw = new MyWindow();
 
