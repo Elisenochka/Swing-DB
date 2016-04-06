a133e67146e69408cc543765c405a197e8043c3e
Elina <eli.lisenochka@gmail.com>
2016-04-06 23:12:07 +0300

    create

--- MainClass.java
+++ MainClass.java
@@ -4,23 +4,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.*;
+import java.util.Arrays;
 
 
 public class MainClass {
     static String name = "Swing.db";
-
+static String shName="Swing";
     BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     static String[] values = new String[5];
 
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        DataBase.CrtDB(shName);
         DataBase.Cntn();
         values[0]="fis";
         values[1]="From";
         values[2]="Ire";
         values[3]="Som";
         values[4]="890823";
-        DataBase.WriteDB(name,values);
+        System.out.println(Arrays.toString(values));
+        DataBase.WriteDB(name);
 
 
         MyWindow mw = new MyWindow();
