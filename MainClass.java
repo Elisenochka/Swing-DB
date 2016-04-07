19a3ea05993f6ac37d97e598666e34fa091f91ea
Elina <eli.lisenochka@gmail.com>
2016-04-07 16:13:44 +0300

    writeDB try

--- MainClass.java
+++ MainClass.java
@@ -10,20 +10,24 @@ import java.util.Arrays;
 public class MainClass {
     static String name = "Swing.db";
 static String shName="Swing";
+    static String tableName = "DataBase";
     BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-    static String[] values = new String[5];
-
+    static String[] logins = new String[5];
+    static String[] firstNames = new String[5];
+    static String[] names = new String[5];
+    static String[] lastNames = new String[5];
+    static Integer[] numbers = new Integer[5];
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        DataBase.CrtDB(shName);
+        //DataBase.CrtDB(shName);
         DataBase.Cntn();
-        values[0]="fis";
-        values[1]="From";
-        values[2]="Ire";
-        values[3]="Som";
-        values[4]="890823";
-        System.out.println(Arrays.toString(values));
-        DataBase.WriteDB(name);
+        logins[0]="fis";
+        firstNames[0]="From";
+        names[0]="Ire";
+        lastNames[0]="Som";
+        numbers[0]=890823;
+        //System.out.println(Arrays.toString(values));
+        DataBase.WriteDB(tableName,logins,firstNames,names,lastNames,numbers);
 
 
         MyWindow mw = new MyWindow();
