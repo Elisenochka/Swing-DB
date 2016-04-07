bbbd4a5b687ddc956e9f203cb95c87de8c304d94
Elina <eli.lisenochka@gmail.com>
2016-04-07 16:13:29 +0300

    writeDB rewritten

--- DataBase.java
+++ DataBase.java
@@ -24,20 +24,36 @@ public class DataBase {
         cnn = null;
         Class.forName("org.sqlite.JDBC");
         cnn = DriverManager.getConnection("jdbc:sqlite:Swing.db");
-        stnt = cnn.createStatement();
+        //stnt = cnn.createStatement();
 
         System.out.println("Base is connected");
     }
 
     public static void CrtDB(String name) throws ClassNotFoundException, SQLException{
-
-        stnt.execute("CREATE TABLE if not exsists "+name+ "('ID','Login','First name','Name','Last name', 'Number')");
+        stnt = cnn.createStatement();
+        stnt.execute("CREATE TABLE if not exsists "+name+ " ('ID' INTEGER PRIMARY KEY AUTOINCREMENT,'Login' TEXT,'First name' TEXT,'Name' TEXT,'Last name' TEXT, 'Number' INT)");
 
         System.out.println("DataBase is created");
     }
 
-    public static void WriteDB(String name, String values[]) throws SQLException{
-        stnt.execute("INSERT INTO "+ name+"('Login','First name','Name','Last name', 'Number') VALUES ("+ Arrays.toString(values)+")");
+    public static void WriteDB(String name, String logins[],String firstNames[],String names[],String lastNames[], Integer numbers[]) throws SQLException{
+
+        //for (int i=0;i<logins.length;i++) {
+        int i=0;
+            try {
+                PreparedStatement pStnt=cnn.prepareStatement("INSERT INTO DataBase ('Login','First name','Name','Last name', 'Number') VALUES (?,?,?,?,?)");
+            //pStnt.setString(1,name);
+                pStnt.setString(1,logins[i]);
+                pStnt.setString(2,firstNames[i]);
+                pStnt.setString(3,names[i]);
+                pStnt.setString(4,lastNames[i]);
+                pStnt.setInt(5,numbers[i]);
+                pStnt.execute();
+            //stnt.execute("INSERT INTO " + name + " ('Login','First name','Name','Last name', 'Number') VALUES (" + logins[i] + ", "+ firstNames[i]+", "+ names[i] + ", " +lastNames[i]+", "+numbers[i] +")");
+            } catch (SQLException e) {
+            e.printStackTrace();
+            }
+        //}
 
         System.out.println("Table is filled");
 
